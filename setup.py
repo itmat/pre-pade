345c63a1cee5ab140eba2e23019d54988d0002b4
Mike DeLaurentis <delaurentis@gmail.com>
2013-04-19 11:32:21 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='prepade',
+      version='0.0.1a1',
+      author='Mike DeLaurentis',
+      author_email='midel@upenn.edu',
+#      url='https://github.com/itmat/pade',
+#      scripts=['bin/pade'],
+      packages=['prepade'],
+#      install_requires=['jinja2', 'matplotlib', 'scipy', 'numpy', 'redis', 'celery', 'h5py', 'flask', 'Flask-WTF'],
+ #     package_data={
+ #       'pade.http.static.css' : ['*.css'],
+ #       'pade.http.templates' : ['*'],
+ #       },
+      
+ #     data_files=[
+ #       ('/etc/', ['pade/padeconfig.yaml'])
+ #       ]
+      )
