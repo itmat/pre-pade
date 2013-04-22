193f5aa6f0c538769891d2dd25d0e3767bc90aa9
Mike DeLaurentis <delaurentis@gmail.com>
2013-04-22 12:39:49 -0400

    Add bsearch_boundary

--- prepade/quantifyexons.py
+++ prepade/quantifyexons.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
+from __future__ import print_function, division
 
 import sys
 import pysam
@@ -19,24 +19,96 @@ def read_chr_to_genes(fh):
         res[chr_].append(gene)
     return res
 
+def process_sam_rec(rec):
+    print("Cigar: " + str(rec.cigar))
+#    cigar = remove_ds(rec.cigarstring)
+#    spans = cigar_to_spans(cigar, rec.pos)
+
+
+
 def read_sam_file(gene_filename, sam_filename, output_fh):
 
     samfile = pysam.Samfile(sam_filename)
 
     with open(gene_filename) as fh:
-        for (chr_, gene) in parse_rum_index_genes(fh):
-            exons = gene.sub_features
-            for exon in exons:
-                start = exon.location.start
-                end   = exon.location.end
-                count = 0
-                for rec in samfile.fetch(chr_, start, end):
-                    count += 1
+        
+        for exon in exons:
+            start = exon.location.start
+            end   = exon.location.end
+            count = 0
+            for rec in samfile.fetch(chr_, start, end):
+                process_sam_rec(rec)
+                count += 1
 
-                if count > 0:
-                    print(chr_, start, end, count, sep="\t", file=output_fh)
+            if count > 0:
+                print(chr_, start, end, count, sep="\t", file=output_fh)
 
 CigarElem = namedtuple('CigarElem', ['count', 'op'])
+
+class FeatureIndex(object):
+    
+    def __init__(self, features):
+        self.features = sorted(features)
+
+        
+def bsearch_boundary(items, key, f=cmp):
+    
+    """Returns the first i for which f(items[i - 1]) < key and f(items[i])
+    >= key. An alternate comparison function can be given as f.
+
+    >>> bsearch_boundary('abcdefg', 'c')
+    2
+
+    >>> bsearch_boundary('cdefg', 'a')
+    0
+    
+    >>> bsearch_boundary('abcde', 'g')
+    5
+
+    >>> bsearch_boundary('abfg', 'e')
+    2
+
+    """
+
+    p = 0
+    q = len(items)
+
+
+    while True:
+
+        r = p + ((q - p) // 2)
+
+        # Find the smallest i where item i-1's end is less than my end
+        # and item i's end is greater than or equal to than my end.
+
+        if r == 0 or f(items[r - 1], key) < 0:
+
+            #            key
+            # [ ... prev this ...]
+            if f(items[r], key) >= 0:
+                return r
+
+            #                   key
+            # [ ... prev this ] 
+            elif r == len(items) - 1:
+                return len(items)
+
+            #                  key
+            # [ ... prev this ...  ]
+            else:
+                p = r
+
+        # key
+        #    last
+        else:
+            q = r
+
+def bsearch_range(items, key, f_left, f_right):
+    p = bsearch(items, key, f_left)
+    q = bsearch(items, key, f_right)
+    return items[p:q]
+
+    
 
 def parse_cigar(cigar):
     pat = re.compile("\d+[A-Z]")
@@ -113,10 +185,14 @@ def remove_ds(cigar):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--rum-gene-info')
+    parser.add_argument('--rum-gene-info', type=file)
     parser.add_argument('samfile')
     parser.add_argument('--output', '-o', type=argparse.FileType('w'))
     args = parser.parse_args()
+
+    chr_to_genes = read_chr_to_genes(args.rum_gene_info)
+
+
 
     read_sam_file(args.rum_gene_info, args.samfile, args.output)
     
