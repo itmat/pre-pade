77c1bb49559dd69318a5246bf91a692f4f6860be
Mike DeLaurentis <delaurentis@gmail.com>
2013-04-30 14:09:56 -0400

    Remove stuff from setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,6 @@ setup(name='prepade',
       version='0.0.1a1',
       author='Mike DeLaurentis',
       author_email='midel@upenn.edu',
-#      url='https://github.com/itmat/pade',
-#      scripts=['bin/pade'],
+      url='https://github.com/itmat/pre-pade',
       packages=['prepade'],
-#      install_requires=['jinja2', 'matplotlib', 'scipy', 'numpy', 'redis', 'celery', 'h5py', 'flask', 'Flask-WTF'],
- #     package_data={
- #       'pade.http.static.css' : ['*.css'],
- #       'pade.http.templates' : ['*'],
- #       },
-      
- #     data_files=[
- #       ('/etc/', ['pade/padeconfig.yaml'])
- #       ]
       )
