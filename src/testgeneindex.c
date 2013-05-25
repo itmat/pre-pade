e2e0616558a8562e77784da6a4386411a562ce2c
Mike DeLaurentis <delaurentis@gmail.com>
2013-05-25 10:29:12 -0400

    Add some tests

--- src/testgeneindex.c
+++ src/testgeneindex.c
@@ -17,24 +17,15 @@ void add_assertion(int passed, char *name) {
   assertion->passed = passed;
 }
 
-
-
 void assert_equals(int a, int b, char *name) {
   add_assertion(a == b, name);
 }
 
-void test_create_index() {
-  struct ExonDB exondb;
-  parse_gtf_file(&exondb, "testdata/arabidopsis.gtf");
-  index_exons(&exondb);  
-  assert_equals(30000, exondb.exons_len, "Number of exons loaded");
-  
+void assert_str_equals(char *a, char *b, char *name) {
+  add_assertion(!strcmp(a, b), name);
 }
 
-
-int main (int argc, char **argv) {
-  test_create_index();
-
+int check_results() {
   int failures = 0;
 
   int i;
@@ -54,5 +45,33 @@ int main (int argc, char **argv) {
     return 1;
   }
   return 0;
-
 }
+
+void test_create_index() {
+  struct ExonDB exondb;
+  parse_gtf_file(&exondb, "testdata/arabidopsis.gtf");
+  index_exons(&exondb);  
+  assert_equals(13214, exondb.exons_len, "Number of exons loaded");
+
+  struct Exon *exon = exondb.exons;
+  assert_str_equals("1", exon->chrom, "First exon chromosome");
+  assert_equals(28692193, exon->start, "First exon start");
+  assert_equals(28692362, exon->end, "First exon end");
+  assert_str_equals("protein_coding", exon->source, "First exon source");
+  assert_str_equals("exon", exon->feature, "First exon feature");
+}
+
+
+int main (int argc, char **argv) {
+  test_create_index();
+
+  return check_results();
+  
+}
+
+
+/*
+	protein_coding	exon	28692193	28692362
+	.	-	.	 gene_id "AT1G76480"; transcript_id "AT1G76480.1"; exon_number "6"; gene_name "F14G6.8"; transcript_name "F14G6.8-202"; seqedit "false";
+
+*/
