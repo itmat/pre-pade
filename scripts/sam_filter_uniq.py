458b781efc4e40532b1b4d11e8978b2eb2a628fa
khayer <katharinaehayer@gmail.com>
2013-09-26 16:58:39 -0400

    local changes

--- scripts/sam_filter_uniq.py
+++ scripts/sam_filter_uniq.py
@@ -74,6 +74,7 @@ def get_next_alignments(samfile,last_entry):
         entries.append(last_entry)
         last_entry = None
     return (entries,last_entry)
+
 def determine_entries_status(entries):
     count = 0
     status = 'unmapped'
