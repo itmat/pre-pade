a0827025d028c3e330a9866bb10196359e05efee
khayer <katharinaehayer@gmail.com>
2013-10-03 11:08:03 -0400

    hitting the pause button

--- scripts/anova.rb
+++ scripts/anova.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 require 'csv'
 require 'rsruby'
+require 'optparse'
+require 'logger'
 
 # Functions
 ####################################################
@@ -18,5 +20,58 @@ R.eval_R("avgData <- function(d) {
   dd
 }")
 
+R.anova("runANOVA <- function(d) {
+  
+}")
+
 ####################################################
+
+
+# Initialize logger
+def setup_logger(loglevel)
+  logger = Logger.new(STDERR)
+  case loglevel 
+  when "debug"
+    logger.level = Logger::DEBUG
+  when "warn"
+    logger.level = Logger::WARN 
+  when "info"
+    logger.leve = Logger::INFO 
+  else
+    logger.level = Logger::ERROR
+  end
+  logger
+end
+
+def setup_options(args)
+  options = {}
+
+  opt_parser = OptionParser.new do |opts|
+    opts.banner = "Usage: anova.rb [options]"
+    opts.separator ""
+    opts.on("-g", "--gtf_file [GTF_FILE]",:REQUIRED,String,  "gtf_file with gene annotation") do |gtf_file|
+      options[:gtf_file] = gtf_file
+    end
+
+    opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+      options[:verbose] = "info"
+    end
+
+    opts.on("-d", "--debug", "Run in debug mode") do |v|
+      options[:verbose] = "debug"
+    end
+  end
+
+  opt_parser.parse!(args)
+  
+  options
+end
+
+
+options = setup_options(ARGV)
+logger = setup_logger(options[:verbose])
+logger.debug(options)
+logger.debug(ARGV)
+
+
 
