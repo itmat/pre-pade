a738316da752771b7b93ee7bcb8be5a2ce241b6d
Katharina Hayer <katharinaehayer@gmail.com>
2013-10-03 13:57:00 -0400

    added anova script

--- scripts/anova.rb
+++ scripts/anova.rb
@@ -4,7 +4,7 @@ require 'rsruby'
 require 'optparse'
 require 'logger'
 
-# Functions
+# R Functions
 ####################################################
 
 R = RSRuby.instance()
@@ -20,23 +20,22 @@ R.eval_R("avgData <- function(d) {
   dd
 }")
 
-R.anova("runANOVA <- function(d) {
-  
+R.eval_R("runAnova <- function(info,csv_file) {
+  info
 }")
 
 ####################################################
 
-
 # Initialize logger
 def setup_logger(loglevel)
   logger = Logger.new(STDERR)
-  case loglevel 
+  case loglevel
   when "debug"
     logger.level = Logger::DEBUG
   when "warn"
-    logger.level = Logger::WARN 
+    logger.level = Logger::WARN
   when "info"
-    logger.leve = Logger::INFO 
+    logger.leve = Logger::INFO
   else
     logger.level = Logger::ERROR
   end
@@ -54,24 +53,110 @@ def setup_options(args)
     end
 
     opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-      options[:verbose] = "info"
+      options[:log_level] = "info"
     end
 
     opts.on("-d", "--debug", "Run in debug mode") do |v|
-      options[:verbose] = "debug"
+      options[:log_level] = "debug"
     end
   end
 
   opt_parser.parse!(args)
-  
+
   options
 end
 
+class Gene
 
-options = setup_options(ARGV)
-logger = setup_logger(options[:verbose])
-logger.debug(options)
-logger.debug(ARGV)
+  def initialize(name, chr, starts, ends, length)
+    @name = name
+    @chr = chr
+    @starts = starts
+    @ends = ends
+    @length = length
+  end
 
+  attr_accessor :name, :chr, :starts, :ends, :length
+
+end
+
+class HTSseq
+
+  def initialize(filename)
+    @filename = filename
+  end
+
+  attr_accessor :filename
+
+
+
+end
+
+def calc_length(starts,ends)
+  length = 0
+  starts.each_with_index do |start,i|
+    length += ends[i] - start
+  end
+  length
+end
+
+def get_genes(gtf_file)
+  genes = []
+  last_name = ""
+  starts = ""
+  chr = ""
+  starts = []
+  ends = []
+  length = 0
+
+  File.open(gtf_file).each do |line|
+    line.chomp!
+    fields = line.split("\t")
+    next unless fields[2] == "exon"
+    name = fields[-1].split("transcript_id \"")[1].split("\"")[0]
+    last_name = name if last_name == ""
+    chr = fields[0] if chr == ""
+    if name != last_name
+      length = calc_length(starts,ends)
+      gene = Gene.new(last_name,chr,starts,ends,length)
+      genes << gene
+      starts = []
+      ends = []
+      chr = fields[0]
+      length = 0
+      last_name = name
+    end
+    starts << fields[3].to_i
+    ends << fields[4].to_i
+    # First collect information
+
+  end
+  length = calc_length(starts,ends)
+  gene = Gene.new(last_name,chr,starts,ends,length)
+  genes << gene
+  genes
+end
+
+def run(argv)
+  options = setup_options(argv)
+  logger = setup_logger(options[:log_level])
+  logger.debug(options)
+  logger.debug(argv)
+
+  genes = get_genes(options[:gtf_file])
+
+
+  argv.each do |file|
+
+  end
+end
+
+#run(ARGV)
+
+
+
+
+#q_values = R.getQvalue(p_values)
+#R.scaleData(numbers).map {|e| e.to_i }
 
 
